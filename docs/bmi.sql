441acba4682e484f4dacbc11d4c254ddd94e5722
Dax Westerman <dax.westerman@vanderbilt.edu>
2020-11-10 11:13:44 -0600

    .

--- docs/bmi.sql
+++ docs/bmi.sql
@@ -5,64 +5,64 @@ CREATE  VIEW ALINE_BMI as
 with ce_wt as
 (
     SELECT
-      co.icustay_id
+      co."ICUSTAY_ID"
       -- we take the median value from their stay
       -- TODO: eliminate obvious outliers if there is a reasonable weight
       -- (e.g. weight of 180kg and 90kg would remove 180kg instead of taking the median)
-      , percentile_cont(0.5) WITHIN GROUP (ORDER BY valuenum) as Weight_Admit
+      , percentile_cont(0.5) WITHIN GROUP (ORDER BY "VALUENUM") as Weight_Admit
     FROM aline_cohort co
     inner join chartevents c
-        on c.subject_id = co.subject_id
-        and c.charttime between co.vent_starttime - interval '1' day and co.vent_starttime
-    WHERE c.valuenum IS NOT NULL
-    AND c.itemid in (762,226512) -- Admit Wt
-    AND c.valuenum != 0
-    group by co.icustay_id
+        on c."SUBJECT_ID" = co."SUBJECT_ID"
+        and c."CHARTTIME" between co.vent_starttime - interval '1' day and co.vent_starttime
+    WHERE c."VALUENUM" IS NOT NULL
+    AND c."ITEMID" in (762,226512) -- Admit Wt
+    AND c."VALUENUM" != 0
+    group by co."ICUSTAY_ID"
 )
 , dwt as
 (
     SELECT
-      co.icustay_id
-      , percentile_cont(0.5) WITHIN GROUP (ORDER BY valuenum) as Weight_Daily
+      co."ICUSTAY_ID"
+      , percentile_cont(0.5) WITHIN GROUP (ORDER BY "VALUENUM") as Weight_Daily
     FROM aline_cohort co
     inner join chartevents c
-        on c.subject_id = co.subject_id
-        and c.charttime between co.vent_starttime - interval '1' day and co.vent_starttime
-    WHERE c.valuenum IS NOT NULL
-    AND c.itemid in (763,224639) -- Daily Weight
-    AND c.valuenum != 0
-    group by co.icustay_id
+        on c."SUBJECT_ID" = co."SUBJECT_ID"
+        and c."CHARTTIME" between co.vent_starttime - interval '1' day and co.vent_starttime
+    WHERE c."VALUENUM" IS NOT NULL
+    AND c."ITEMID" in (763,224639) -- Daily Weight
+    AND c."VALUENUM" != 0
+    group by co."ICUSTAY_ID"
 )
 , ce_ht0 as
 (
     SELECT
-      co.icustay_id
+      co."ICUSTAY_ID"
       , case
         -- convert inches to centimetres
-          when itemid in (920, 1394, 4187, 3486)
-              then valuenum * 2.54
-            else valuenum
+          when "ITEMID" in (920, 1394, 4187, 3486)
+              then "VALUENUM" * 2.54
+            else "VALUENUM"
         end as Height
     FROM aline_cohort co
     inner join chartevents c
-        on c.subject_id = co.subject_id
-        and c.charttime <= co.outtime
-    WHERE c.valuenum IS NOT NULL
-    AND c.itemid in (226730,920, 1394, 4187, 3486,3485,4188) -- height
-    AND c.valuenum != 0
+        on c."SUBJECT_ID" = co."SUBJECT_ID"
+        and c."CHARTTIME" <= co."OUTTIME"
+    WHERE c."VALUENUM" IS NOT NULL
+    AND c."ITEMID" in (226730,920, 1394, 4187, 3486,3485,4188) -- height
+    AND c."VALUENUM" != 0
 )
 , ce_ht as
 (
     SELECT
-        icustay_id
+        "ICUSTAY_ID"
         -- extract the median height from the chart to add robustness against outliers
         , percentile_cont(0.5) WITHIN GROUP (ORDER BY height) as Height_chart
     from ce_ht0
-    group by icustay_id
+    group by "ICUSTAY_ID"
 )
 , echo as
 (
-    select icustay_id
+    select "ICUSTAY_ID"
         , 2.54*height_first as height_echo
         , 0.453592*weight_first as weight_echo
     from aline_echodata ec
@@ -70,7 +70,7 @@ with ce_wt as
 , bmi as
 (
 select
-    co.icustay_id
+    co."ICUSTAY_ID"
     -- weight in kg
     , round(cast(
           coalesce(ce_wt.Weight_Admit, dwt.Weight_Daily, ec.weight_echo)
@@ -91,22 +91,22 @@ from aline_cohort co
 
 -- admission weight
 left join ce_wt
-    on co.icustay_id = ce_wt.icustay_id
+    on co."ICUSTAY_ID" = ce_wt."ICUSTAY_ID"
 
 -- daily weights
 left join dwt
-    on co.icustay_id = dwt.icustay_id
+    on co."ICUSTAY_ID" = dwt."ICUSTAY_ID"
 
 -- height
 left join ce_ht
-    on co.icustay_id = ce_ht.icustay_id
+    on co."ICUSTAY_ID" = ce_ht."ICUSTAY_ID"
 
 -- echo data
 left join echo ec
-    on co.icustay_id = ec.icustay_id
+    on co."ICUSTAY_ID" = ec."ICUSTAY_ID"
 )
 select
-    icustay_id
+    "ICUSTAY_ID"
     , case
         when weight is not null and height is not null
             then (weight / (height*height))
@@ -122,4 +122,4 @@ select
     , Height_echo
     , Weight_echo
 from bmi
-order by icustay_id;
+order by "ICUSTAY_ID";
